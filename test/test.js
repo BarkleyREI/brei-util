37f8b6e5baafeab1e27dee1ab200c660679e8e06
Ian Moffitt <IMoffitt@barkleyrei.com>
2020-05-01 14:59:32 -0400

    chore(test): Added some real tests.

--- test/test.js
+++ test/test.js
@@ -5,7 +5,48 @@
 const util = require('../index.js');
 
 describe('brei-util -- Test (test/test.js)', function () {
-	it('Empty test', function () {
+	it('Test assert', function () {
 		util.assert(true);
+	});
+
+	it('Test deep equal', function () {
+		let obj1 = {
+			'test': 'foo',
+			'bar': {
+				'baz': 1
+			}
+		};
+		let obj2 = {
+			'test': 'foo',
+			'bar': {
+				'baz': 1
+			}
+		};
+		let obj3 = {
+			'test': 'foo',
+			'bar': {
+				'baz': 2
+			}
+		};
+
+		util.assert(util.deep(obj1, obj2));
+		util.assert(!util.deep(obj1, obj3));
+	});
+
+	it('Test deep object diff', function () {
+		let obj1 = {
+			'test': 'foo',
+			'bar': {
+				'baz': 1
+			}
+		};
+		let obj2 = {
+			'test': 'foo',
+			'bar': {
+				'baz': 1
+			}
+		};
+
+		util.assert(util.deepNotOnly(obj1, obj2) !== {});
 	});
 });
